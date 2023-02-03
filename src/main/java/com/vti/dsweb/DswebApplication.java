5f4b883c04f4dbd8a298169bb4dc885f731f7ed8
vandoaderson <vandoaderson.s@gmail.com>
2023-02-02 21:55:39 -0300

    Commit inicial

---
+++ src/main/java/com/vti/dsweb/DswebApplication.java
@@ -0,0 +1,13 @@
+package com.vti.dsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vti/dsweb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vti.dsweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DswebApplication.class);
+	}
+
+}
